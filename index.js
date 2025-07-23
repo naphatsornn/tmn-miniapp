8a2926ec5e21e86912aa9c4e163e87733d316afe
naphatsornn <naphatsorn.ki@ku.th>
2025-07-23 13:32:12 +0700

    Initial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   const headers = req.headers;
 
   // แสดง header ทั้งหมดในรูปแบบ HTML
-  let html = `<h1>Headers:</h1><ul>`;
+  let html = `<h1>Headers : </h1><ul>`;
   for (const key in headers) {
     html += `<li><strong>${key}</strong>: ${headers[key]}</li>`;
   }
