ccdbe63a567a7e529eeae3b49870e7472aecb39b
naphatsornn <naphatsorn.ki@ku.th>
2025-07-23 12:51:37 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const headers = req.headers;
+
+  // แสดง header ทั้งหมดในรูปแบบ HTML
+  let html = `<h1>Headers:</h1><ul>`;
+  for (const key in headers) {
+    html += `<li><strong>${key}</strong>: ${headers[key]}</li>`;
+  }
+  html += `</ul>`;
+
+  res.send(html);
+});
+
+module.exports = app;
